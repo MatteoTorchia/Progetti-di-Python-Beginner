1f45e3f9aee78d6ee013f9392dd2baefb641c309
Matteo Torchia <matteotorchia.student@gmail.com>
2025-11-12 19:01:59 +0100

    create tic_tac_toe and its temp

---
+++ temp- for tic_tac_toe.py
@@ -0,0 +1,13 @@
+
+def make_list_of_free_fields(board):
+    # The function browses the board and builds a list of all the free squares; 
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+
+
+def victory_for(board, sign):
+    # The function analyzes the board's status in order to check if 
+    # the player using 'O's or 'X's has won the game
+
+
+def draw_move(board):
+    # The function draws the computer's move and updates the board.
---
+++ tic_tac_toe.py
@@ -0,0 +1,43 @@
+board = [
+    [1, 2, 3],
+    [4, "X", 6],
+    [7, 8, 9]
+]
+# --- Functions ---
+def display_board(board):
+    for i in board:
+        print("+-------+-------+-------+")
+        print("|       |       |       |")
+        print(f"|   {i[0]}   |   {i[1]}   |   {i[2]}   |")
+        print("|       |       |       |")
+    print("+-------+-------+-------+")
+
+def enter_move(board):
+    move = int(input(print("Select a square between 1 and 9: ")))
+    try:
+        for i in board:
+            for j in i:
+                if move == j:
+                    j = move
+                    break
+        else:
+            print("Error: You inserted an illegal value. Try again")
+    except ValueError:
+        print("Error: You inserted an illegal value.")
+    # The function accepts the board's current status, asks the user about their move, 
+    # checks the input, and updates the board according to the user's decision.
+
+
+# --- Main Flow ---
+display_board(board)
+enter_move(board)
+
+display_board(board)
+enter_move(board)
+
+display_board(board)
+enter_move(board)
+
+display_board(board)
+enter_move(board)
+
