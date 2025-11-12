4ab7c848a6d29c25d59089e70083fcb294ca4676
Matteo Torchia <matteotorchia.student@gmail.com>
2025-11-12 20:47:38 +0100

    minor edits

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,20 +10,36 @@ def display_board(board):
         print("|       |       |       |")
         print(f"|   {i[0]}   |   {i[1]}   |   {i[2]}   |")
         print("|       |       |       |")
-    print("+-------+-------+-------+")
+    print("+-------+-------+-------+\n")
 
 def enter_move(board):
-    move = int(input(print("Select a square between 1 and 9: ")))
-    try:
+    while True:
+        move = input("Select a square between 1 and 9: ")
+        try:
+            move = int(move)
+        except ValueError:
+            print("Error: You inserted an illegal value. Try again")
         for i in board:
             for j in i:
                 if move == j:
-                    j = move
-                    break
+                    j = "O"
+                    return board
         else:
-            print("Error: You inserted an illegal value. Try again")
-    except ValueError:
-        print("Error: You inserted an illegal value.")
+            print("Error: You inserted an illegal value. Try again")   
+
+def enter_move_WRONG(board):
+    while True:
+        move = input("Select a square between 1 and 9: ")
+        try:
+            for i in board:
+                for j in i:
+                    if move == j:
+                        j = "O"
+                        return board
+            else:
+                print("Error: You inserted an illegal value. Try again")
+        except ValueError:
+            print("Error: You inserted an illegal value.")
     # The function accepts the board's current status, asks the user about their move, 
     # checks the input, and updates the board according to the user's decision.
 
