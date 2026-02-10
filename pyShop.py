52b6abe54f1c0da1c0c6b5b8abfacc2d472e81d4
Matteo Torchia <matteotorchia.student@gmail.com>
2026-02-10 22:46:27 +0100

    while loop

--- pyShop.py
+++ pyShop.py
@@ -1,17 +1,21 @@
 catalog = {"apple": 0.5, "orange": 0.6, "bread": 1.0, }
-cart = ()
-flag=0
+cart = []
+
 
 # --- Main Flow ---
 print("--- Welcome to the shop! ---")
+
 for i in catalog:
     print(i, catalog[i])
-while flag==0:
-    request = input("What do you want to buy? ")
-    for i in catalog:
-        if request == i:
-            n = input(f"How many {i} do you want? ")
-            flag = 1
+
+while True:
+        request = input("What do you want to buy? (type 'stop' to end) ")
+
+        if request == "stop":
             break
-        if i == len(catalog)-1:
+        if request in catalog:
+            cart.append(request)
+            print(f"Item '{request}' added to cart!")
+        else:
             print(f"There is no item such as '{request}'\nTry something different.")
+
